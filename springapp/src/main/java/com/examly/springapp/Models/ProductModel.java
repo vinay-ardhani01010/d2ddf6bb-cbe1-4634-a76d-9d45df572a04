fdf669103f13cbe509a2ded02f6e3c497092ff49
abhi4mu <abhinavmunagala4@gmail.com>
2021-09-29 14:37:14 +0000

    Added two models

---
+++ springapp/src/main/java/com/examly/springapp/Models/ProductModel.java
@@ -0,0 +1,13 @@
+package com.examly.springapp.Models;
+
+@Entity
+public class ProductModel {
+    
+    private String productId;
+    private String imageUrl;
+    private String productName;
+    private String price;
+    private String description;
+    private String quantity;
+
+}
